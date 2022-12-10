dfdbc5e76e79f43068691c49ab4230b53f34ae60
norman santiago <nsantiago2719@gmail.com>
2022-12-10 18:13:03 +0800

    change String to str

--- src/util.rs
+++ src/util.rs
@@ -8,11 +8,19 @@ pub fn get_input_value(d: &str) -> String {
   trim_newline(&value)
 }
 
-pub fn trim_newline(s: &String) -> String {
+pub fn trim_newline(s: &str) -> String {
   s.trim_end_matches(&['\r', '\n']).to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn remove_newline() {
+      let sample_string = "y\r\n";
+      assert_eq!(trim_newline(&sample_string), "y")
+    }
+
 }
